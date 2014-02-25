09f4c7fca1ef9bb71b9f1f79203013c6c845ae4d
Johannes Egger <eggerjohansi@gmail.com>
2014-02-25 10:02:59 +0100

    Support zooming in detail view.

--- WindowsStore/Pages/ShowDocumentPage.xaml.cs
+++ WindowsStore/Pages/ShowDocumentPage.xaml.cs
@@ -7,6 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
@@ -31,6 +33,7 @@ namespace MyDocs.WindowsStore.Pages
             base.OnNavigatedTo(e);
             dtm = DataTransferManager.GetForCurrentView();
             dtm.DataRequested += dtm_DataRequested;
+            Window.Current.SizeChanged += Window_SizeChanged;
         }
 
         private void dtm_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
@@ -61,11 +64,39 @@ namespace MyDocs.WindowsStore.Pages
         {
             base.OnNavigatedFrom(e);
             dtm.DataRequested -= dtm_DataRequested;
+            Window.Current.SizeChanged -= Window_SizeChanged;
         }
 
         private void backButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             NavigationHelper.GoBack();
+        }
+
+        // http://stackoverflow.com/questions/14667556/a-simple-photo-album-with-pinch-and-zoom-using-flipview
+        private void Window_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            // Reset scroll view size
+            var flipViewItems = Enumerable.Range(0, imageFlipView.Items.Count)
+                .Select(i => imageFlipView.ContainerFromIndex(i))
+                .Where(container => container != null);
+            foreach (var flipViewItem in flipViewItems) {
+                var scrollViewItem = VisualTreeHelperEx.GetChildren(flipViewItem).OfType<ScrollViewer>().First();
+                scrollViewItem.Width = e.Size.Width;
+                scrollViewItem.Height = e.Size.Height; // Reset width and height to match the new size
+                System.Diagnostics.Debug.WriteLine(scrollViewItem.Width);
+                System.Diagnostics.Debug.WriteLine(scrollViewItem.Height);
+                System.Diagnostics.Debug.WriteLine(e.Size);
+                scrollViewItem.ChangeView(0, 0, 1, true);
+            }
+        }
+
+        // http://stackoverflow.com/questions/14667556/a-simple-photo-album-with-pinch-and-zoom-using-flipview
+        private void FlipView_SelectionChanged(object sender, Windows.UI.Xaml.Controls.SelectionChangedEventArgs e)
+        {
+            var flipView = (FlipView)sender;
+            var flipViewItem = flipView.ContainerFromIndex(flipView.SelectedIndex);
+            var scrollViewItem = VisualTreeHelperEx.GetChildren(flipViewItem).OfType<ScrollViewer>().First();
+            scrollViewItem.ChangeView(0, 0, 1, true);
         }
     }
 }
