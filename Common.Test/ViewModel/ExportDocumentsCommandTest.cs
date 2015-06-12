18e07dee1efa7c383cbd9cd25e1736a12ed22593
Johannes Egger <eggerjohansi@gmail.com>
2015-06-12 22:34:35 +0200

    Add tests for `ExportDocumentsCommand`

---
+++ Common.Test/ViewModel/ExportDocumentsCommandTest.cs
@@ -0,0 +1,38 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyDocs.Common.Contract.Page;
+using MyDocs.Common.Contract.Service;
+using MyDocs.Common.Model;
+using System;
+
+namespace MyDocs.Common.Test.ViewModel
+{
+    public partial class DocumentViewModelTest
+    {
+        [TestMethod]
+        public void ExportDocumentsCommandShouldBeEnabled()
+        {
+            var sut = CreateSut();
+
+            sut.ExportDocumentsCommand.CanExecute(null).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void ExportDocumentsCommandShouldFailWhenUnlicensed()
+        {
+            var licenseService = A.Fake<ILicenseService>();
+            var exportDocumentsService = A.Fake<IExportDocumentService>();
+            A.CallTo(() => licenseService.Unlock(A<string>._)).Throws(new LicenseStatusException("Test", LicenseStatus.Locked));
+            var sut = CreateSut(licenseService: licenseService, exportDocumentService: exportDocumentsService);
+
+            using (Fake.CreateScope())
+            {
+                sut.ExportDocumentsCommand.Execute(null);
+                // TODO find a better way to wait until the async command has finished
+                System.Threading.Tasks.Task.Delay(TimeSpan.FromMilliseconds(500)).Wait();
+                A.CallTo(() => exportDocumentsService.ExportDocuments()).MustNotHaveHappened();
+            }
+        }
+    }
+}
