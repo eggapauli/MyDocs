b0f6f7037933ba2e8a173a19ef95141c87b58a32
Johannes Egger <eggerjohansi@gmail.com>
2015-06-20 22:02:28 +0200

    Add tests for `AddPhotoFromFileCommand`

--- Common.Test/ViewModel/AddPhotoFromFileCommandTest.cs
+++ Common.Test/ViewModel/AddPhotoFromFileCommandTest.cs
@@ -59,13 +59,57 @@ namespace MyDocs.Common.Test.ViewModel
         [TestMethod]
         public void ShouldSkipFilesWherePageExtractionFails()
         {
-            Assert.Inconclusive();
+            var files = new[]
+            {
+                new Uri("ms-appx:///Images/UnitTestLogo.scale-100.png"),
+                new Uri("ms-appx:///Images/UnitTestSmallLogo.scale-100.png")
+            };
+            var filePicker = CreateFilePickerMock(files);
+            var pageExtractor = new PageExtractorMock
+            {
+                SupportsExtensionFunc = delegate { return true; },
+                ExtractPagesFunc = (f, _) =>
+                {
+                    if (f.Name == files[0].Segments.Last())
+                    {
+                        throw new Exception("Test");
+                    }
+                    var pages = Enumerable.Repeat(new Model.Logic.Photo(f), 1);
+                    return Task.FromResult(pages);
+                }
+            };
+
+            var sut = CreateSut(filePicker: filePicker, pageExtractor: pageExtractor);
+            sut.EditingDocument = new Document();
+            sut.AddPhotoFromFileCommand.Execute(null);
+            WaitForCommand();
+
+            sut.EditingDocument.SubDocuments.Count.Should().Be(1);
         }
 
         [TestMethod]
         public void ViewModelShouldBeBusyWhileExtractingPages()
         {
-            Assert.Inconclusive();
+            var file = new Uri("ms-appx:///Images/UnitTestLogo.scale-100.png");
+            var filePicker = CreateFilePickerMock(new[] { file });
+            var tcs = new TaskCompletionSource<IEnumerable<Model.Logic.Photo>>();
+            var pageExtractor = new PageExtractorMock
+            {
+                SupportsExtensionFunc = delegate { return true; },
+                ExtractPagesFunc = delegate { return tcs.Task; }
+            };
+
+            var sut = CreateSut(filePicker: filePicker, pageExtractor: pageExtractor);
+            sut.EditingDocument = new Document();
+            sut.IsBusy.Should().BeFalse();
+
+            sut.AddPhotoFromFileCommand.Execute(null);
+            WaitForCommand();
+            sut.IsBusy.Should().BeTrue();
+
+            tcs.SetResult(Enumerable.Empty<Model.Logic.Photo>());
+            WaitForCommand();
+            sut.IsBusy.Should().BeFalse();
         }
 
         [TestMethod]
